9774730b5aa9d3cf0a6e407e97337d04348426d1
chosunme96 <chosunme96@naver.com>
2018-05-14 11:45:49 +0000

    backend_01

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,35 @@
 class PostController < ApplicationController
+
+    def index
+        @posts=Post.all
+    end 
+    
+    def new
+    end
+
+    def create
+        @post=Post.new
+        @post.title=params[:post_title]
+        @post.content=params[:post_content]
+        @post.save
+        
+        redirect_to '/post/index'
+    end
+    
+    def destroy
+        post=Post.find(params[:post_id])
+        post.destroy
+        redirect_to '/post/index'
+    end
+    
+    def edit
+    end
+
+    def update
+    end 
+    
+
+
+
+
 end
--- db/migrate/20180514111402_create_posts.rb
+++ db/migrate/20180514111402_create_posts.rb
@@ -1,8 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string:title
+      t.text:content
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
